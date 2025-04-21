0bbff07be91983fc7802135e22b72b4ccfbcd263
Ravi Kumar Yadav <ravi.yadav@paytm.com>
2025-04-21 22:37:48 +0530

    initial commit for Thymeleaf

--- src/test/java/com/ravi/thymeleafdemo/ThymeleafdemoApplicationTests.java
+++ src/test/java/com/ravi/thymeleafdemo/ThymeleafdemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.ravi.cruddemo;
+package com.ravi.thymeleafdemo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CruddemoApplicationTests {
+class ThymeleafdemoApplicationTests {
 
 	@Test
 	void contextLoads() {
